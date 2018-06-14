be64cedff6a8d039d457ab91002bbd6d2b734231
Deep <dipanjan@umich.edu>
2018-06-14 12:34:59 +0530

    First Commit

---
+++ client.php
@@ -0,0 +1,41 @@
+<?php
+
+require_once ("DBController.php");
+
+$db_handle_deux = new DBController;
+
+$query = "SELECT DISTINCT cpmodule FROM `ibb_cardetails_tracker";
+
+
+$result = $db_handle_deux->runQuery($query);
+
+?>
+
+<html>
+
+    <head></head>
+
+    <body>
+        <div class="row">
+                <label>Client:</label><br /> 
+                    <select
+                    id="client-list" name="client-list" 
+                    class="demoInputBox"
+                     multiple size=4>
+
+                        <option value="">Select Client</option>
+                            <?php
+                                foreach ($result as $country) {
+                            ?>
+                        <option value="<?php echo $country["cpmodule"]; ?>"><?php echo $country["cpmodule"]; ?></option>
+                            <?php
+                                }
+                            ?>
+                    </select>
+        </div>
+
+
+
+    </body>
+
+</html>
